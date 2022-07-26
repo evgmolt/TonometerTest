8be87d5d94bc49cc9c4956f9bf9cf3de7c5a2109
Evgenii Molchkov <evg.molt@gmail.com>
2022-07-26 09:22:22 +0400

    Filter 25% bug fixed

--- HRV/Histogram.cs
+++ HRV/Histogram.cs
@@ -10,12 +10,14 @@
         private int _modaAmplitude;
         private int _rangeOfNN;
         private readonly int _SDNN;
+        private readonly int _RMSSD;
         private readonly int _averNN;
         public int[] HistoBuffer = new int[_histoBufferSize];
         public int Moda { get { return _moda; } }                   //ms
         public int ModaAmplitude { get { return _modaAmplitude; } }
         public int RangeOfNN { get { return _rangeOfNN; } }         //ms
         public int SDNN { get { return _SDNN; } }
+        public int RMSSD { get { return _RMSSD; } }
         public int AverNN { get { return _averNN; } }
 
         public Histogram(int[] arrayOfIndexes, int samplingFrequency)
@@ -24,8 +26,13 @@
             
             _NNArray = GetRhythmogram(arrayOfIndexes);
             _averNN = (int)_NNArray.Average();
+            //Вычисление SDNN
             var diffSqrArray = _NNArray.Select(x => (x - _averNN) * (x - _averNN)).ToArray();
             _SDNN = (int)Math.Sqrt(diffSqrArray.Sum() / diffSqrArray.Length);
+            //Вычисление RMSSD
+            var diffSeqNN = _NNArray.Zip(_NNArray.Skip(1), (first, second) => second - first);
+            var diffSeqSqrNN = diffSeqNN.Select(x => x * x);
+            _RMSSD = (int)Math.Sqrt(diffSeqSqrNN.Sum() / diffSeqSqrNN.Count());
             BuildHisto();
         }
 
@@ -35,19 +42,19 @@
             const int LoLimit = 50;   // 240 уд / мин 
             const int HiLimit = 400 ; // 30  уд / мин 
             List<int> result = new();   
-            int prevInterval = arrayOfIndexes[1] - arrayOfIndexes[0]; ;
+            int prevInterval = arrayOfIndexes[1] - arrayOfIndexes[0];
             for (int i = 0; i < arrayOfIndexes.Length - 1; i++)
             {
                 int newInterval = arrayOfIndexes[i + 1] - arrayOfIndexes[i];
-                prevInterval = newInterval;
-                if (newInterval < LoLimit || 
-                    newInterval > HiLimit || 
-                    newInterval > prevInterval + prevInterval / 4 || 
-                    newInterval < prevInterval - prevInterval / 4)
+                if (newInterval < LoLimit || newInterval > HiLimit)
                 {
                     continue;
                 }
-                result.Add(newInterval * msInSecond / _samplingFrequency);
+                if (newInterval < prevInterval + prevInterval / 4 && newInterval > prevInterval - prevInterval / 4)
+                {
+                    result.Add(newInterval * msInSecond / _samplingFrequency);
+                }
+                prevInterval = newInterval;
             }
             return result.ToArray();
         }
